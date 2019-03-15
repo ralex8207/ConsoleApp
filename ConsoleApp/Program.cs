f47fd9c841ba71063f2b554a368b5c6961860a93
Alexander Rodin <ralex8207@gmail.com>
2019-03-15 16:24:26 +0300

    add class

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,10 +2,17 @@
 
 namespace ConsoleApp {
     class Program {
+
+        class MyClass
+        {
+            public string Name { get; set; }
+        }
+
         static void Main(string[] args)
         {
 
             Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world2");
 
             Console.ReadKey();
 
